a9abcd3ff3bb36bf2cdaf1240754e7d1b212a364
du-jiang <39614406+du-jiang@users.noreply.github.com>
2021-03-22 11:11:45 +0800

    Add files via upload

--- douyu简单selenium.py
+++ douyu简单selenium.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-
+import time
 class douyu(object):
     def __init__(self):
         self.url = "https://www.douyu.com/g_LOL"
@@ -9,12 +9,15 @@ class douyu(object):
         room_list = self.driver.find_elements_by_xpath('//li[@class="layout-Cover-item"]/div')
         #获取节点数据
         for room in room_list:
-            temp = {}
-            temp['title'] = room.find_element_by_xpath('./a/div[2]/div[1]/h3').text
-            temp['type'] = room.find_element_by_xpath('./a/div[2]/div[1]/span').text
-            temp['boss'] = room.find_element_by_xpath('./a/div[2]/div[2]/h2/div').text
-            temp['num'] = room.find_element_by_xpath('./a/div[2]/div[2]/span').text
-            print(temp)
+            try:
+                temp = {}
+                temp['title'] = room.find_element_by_xpath('./a/div[2]/div[1]/h3').text
+                temp['type'] = room.find_element_by_xpath('./a/div[2]/div[1]/span').text
+                temp['boss'] = room.find_element_by_xpath('./a/div[2]/div[2]/h2/div').text
+                temp['num'] = room.find_element_by_xpath('./a/div[2]/div[2]/span').text
+                print(temp)
+            except:
+                pass
 
 
     def run(self):
